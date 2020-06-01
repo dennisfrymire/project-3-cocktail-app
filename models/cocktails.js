05e42515983db5ac69afdd4d22943c24049b92e4
zchen92 <zchen92@users.noreply.github.com>
2020-06-01 17:55:57 -0400

    saved merge changes

--- models/cocktails.js
+++ models/cocktails.js
@@ -22,8 +22,5 @@ module.exports = Cocktails;
 //name, first letter, ingredient, cocktail id, ingredient id, random, alcoholic vs nonalcoholic
 
 //object
-<<<<<<< HEAD
 //strDrink, strTag, strAlcoholic,, strGlass, strInstruction, strIngredient1,2,3 etc, strmeasure1, 2, etc, strDrinkThumb, strCategory
-=======
 //strDrink, strTag, strAlcoholic, strGlass, strInstruction, strIngredient1, 2, 3 etc, strmeasure1, 2, etc, strDrinkThumb, strCategory
->>>>>>> fa3e4683413a78fc05c0260db9b576c00b358b26

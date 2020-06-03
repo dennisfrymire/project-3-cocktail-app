83246e1b6d4167376688e3e7cbc6afb267ee6b4a
Leo <hamlap6@gmail.com>
2020-06-02 17:27:59 -0700

    pushign broken code

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,30 @@
+class NewDrinkForm extends React.Component {
+    state = {
+        strDrink: '',
+        strDrinkThumb: ''
+    }
+
+    handleChange = (event) => {
+        // setState is a built-in method of the React library
+        this.setState({
+            [event.target.id]: event.target.value
+        })
+    }
+
+    render(){
+        return (
+                <form className='newDrink' onSubmit={(ev) => this.props.handleSubmit(ev, this.state)}>
+                    <label htmlFor='strDrink'>Drink Name</label>
+                    <input id='strDrink' type='text' value={this.state.strDrink} onChange={this.handleChange}/>
+                    <label htmlFor='strDrinkThumb'>Image Link</label>
+                    <input id='strDrinkThumb' type='text' value={this.state.strDrinkThumb} onChange={this.handleChange}/>
+                    <input type='submit' />
+                </form>
+        )
+    }
+}
+
+
 class App extends React.Component {
     state = {
         cocktails: [],
@@ -21,7 +48,7 @@ class App extends React.Component {
         // this.getData();
     }
 
-    getData = () => {
+    // getData = () => {
         // this.setState({
         //     ingredient: 'margarita',
         //     searchURL: this.state.baseURL + this.state.apikey + this.state.query + this.state.ingredient
@@ -35,7 +62,7 @@ class App extends React.Component {
         //     console.log(data)
         //     this.setState({cocktails: data})
         // })
-    }
+    // }
 
     // handleChange = (event) => {
     //     // setState is a built-in method of the React library
@@ -63,13 +90,36 @@ class App extends React.Component {
     //     })
     // }
 
+    handleSubmit = (event, newFormState) => {
+        event.preventDefault();
+        fetch('/cocktails', {
+            body: JSON.stringify({strDrink: this.state.strDrink, strDrinkThumb: this.state.strDrinkThumb}),
+            method: "POST",
+            headers: {
+                'Accept': 'application/json, text/plain, */*',
+                'Content-Type': 'application/json'
+            }
+        }).then(response => response.json())
+            .then(newDrink => {
+                newDrink.strDrink = newFormState.strDrink
+                newDrink.strDrinkThumb = newFormState.strDrinkThumb
+                console.log(newDrink)
+                this.setState({
+                    cocktails: [...this.state.cocktails, newDrink],
+                    strDrink: '',
+                    strDrinkThumb: ''
+                })
+            })
+    }
+
     render(){
         // this for some reason prints out 2 console logs, one of the cocktails arr and one of just an object containing 5 drinks
         // i suspect the 2nd console log prints with the populated array b/c there is a delay as the brower awaits the fetch, and as the body re-renders it console.logs again
         console.log(this.state.cocktails.drinks)
         return (
             <div>
-                <h1>testing</h1>
+                <h1>Cocktails</h1>
+                <NewDrinkForm cocktails={this.state.cocktails} handleSubmit={this.handleSubmit}/>
                 {/* if cocktails.drinks exists, then print. cocktails.drinks does not exist until after browser loads and half second delay */}
                 {this.state.cocktails.drinks && this.state.cocktails.drinks.map(drink => {
                         return (
